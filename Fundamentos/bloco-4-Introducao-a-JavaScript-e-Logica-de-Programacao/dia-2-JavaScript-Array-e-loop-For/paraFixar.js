cd667442df6f089b85d40cdcfce4dfda347639e6
Lucas Viana Ferreira <lucasviana300@hotmail.com>
2022-02-03 19:51:27 -0300

    exercicios do dia

---
+++ Fundamentos/bloco-4-Introducao-a-JavaScript-e-Logica-de-Programacao/dia-2-JavaScript-Array-e-loop-For/paraFixar.js
@@ -0,0 +1,30 @@
+//let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+//let menuServices = menu[1] ;
+
+//console.log(menuServices);
+//------------------------------------------------------------
+
+//let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+//let indexOfPortfolio = menu.indexOf("Portfólio");
+
+//console.log(indexOfPortfolio);
+//-------------------------------------------------------------
+
+//let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+//menu.push("Contato")
+
+//console.log(menu);
+//---------------------------------------------------------------
+
+//let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+//for(let i = 0; i < groceryList.length; i++)
+//{
+  //  console.log(groceryList[i])
+//}
+//-----------------------------------------------------------------
+
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+for (let nomes of names)
+{
+    console.log(nomes)
+}
