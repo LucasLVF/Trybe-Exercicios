641c4abfb3020509ade94270fc0d38e26d8bfb2b
Lucas Viana Ferreira <lucasviana300@hotmail.com>
2022-03-07 23:51:12 -0300

    coisa pra caramba

---
+++ Fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/myFizzBuzz.test.js
@@ -0,0 +1,26 @@
+/* 3 - A função myFizzBuzz(num) recebe um número num e retorna "fizzbuzz" se o número for divisível por 3 e 5 , retorna "fizz" se for divisível apenas por 3 , retorna "buzz" se divisível apenas por 5 , retorna o próprio número caso não seja divisível por 3 ou 5 e retorna false caso num não seja um número
+Faça uma chamada com um número divisível por 3 e 5 e verifique se o retorno é o esperado
+Faça uma chamada com um número divisível por 3 e verifique se o retorno é o esperado
+Faça uma chamada com um número divisível por 5 e verifique se o retorno é o esperado
+Faça uma chamada com um número que não é divisível por 3 ou 5 e verifique se o retorno é o esperado
+Faça uma chamada com um parâmetro que não é um número e verifique se o retorno é o esperado
+ */
+const myFizzBuzz = require('./myFizzBuzz');
+
+describe('exercicio 3', () =>{
+    it('checando se o resultado é fizzbuzz', ()=>{
+        expect(myFizzBuzz(15)).toEqual('fizzbuzz');
+    })
+    it('checando se o resultado é fizz', ()=>{
+        expect(myFizzBuzz(9)).toEqual('fizz');
+    })
+    it('checando se o resultado é buzz', ()=>{
+        expect(myFizzBuzz(10)).toEqual('buzz');
+    })
+    it('checando se o resultado é o numero passado', ()=>{
+        expect(myFizzBuzz(7)).toEqual(7);
+    })
+    it('checando se o resultado é false', ()=>{
+        expect(myFizzBuzz('a')).toEqual(false);
+    })
+})
